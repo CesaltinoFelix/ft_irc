c8adbe84b003ff77db9aa6826d2ba2d135d1aefe
cesaltinofelix <cesaltinofelix2000@gmail.com>
2026-03-10 19:55:02 +0100

    refactor info and message functions to improve welcome message and error handling

--- cmd/utls.cpp
+++ cmd/utls.cpp
@@ -1,39 +1,39 @@
 #include "Server.hpp"
 void Server::info(int fd)
 {
-	sendToClient(fd, ":User Autenticated and cadastraded");
-	sendToClient(fd, "==================================================");
-	sendToClient(fd, "==================================================");
-	sendToClient(fd, "============= WELCOME TO FT_IRC ===================");
-	sendToClient(fd, "===================================================");
-	sendToClient(fd, "===================================================");
-	sendToClient(fd, "COMANDS");
-	sendToClient(fd, "=========");
-	sendToClient(fd, "KICK");
-	sendToClient(fd, "INVITE");
-	sendToClient(fd, "JOIN");
-	sendToClient(fd, "MODE");
-	sendToClient(fd, "TOPIC");
+	std::string nick = getNickByFd(fd);
+	sendToClient(fd, "001 " + nick + " :Welcome to the FT_IRC Network " + nick);
+	sendToClient(fd, "002 " + nick + " :Your host is ft_irc, running version 1.0");
 }
 
 void Server::message(int fd, std::string args)
 {
+	if (args.empty())
+	{
+		sendToClient(fd, "411 :No recipient given (PRIVMSG)");
+		return;
+	}
 	size_t spacePos = args.find(' ');
 	if (spacePos == std::string::npos)
 	{
-		sendToClient(fd, "411 :No recipient given");
+		sendToClient(fd, "412 :No text to send");
 		return;
 	}
 	std::string target = args.substr(0, spacePos);
 	std::string msg = args.substr(spacePos + 1);
+	if (msg.empty())
+	{
+		sendToClient(fd, "412 :No text to send");
+		return;
+	}
+	if (msg[0] == ':')
+		msg.erase(0, 1);
 	if (target[0] == '#')
 	{
 		cmdPrivmsg(fd, target, msg);
 	}
 	else
 	{
-		if (msg[0] == ':')
-			msg.erase(0, 1);
 		cmdPrivmsg_to_client(fd, target, msg);
 	}
 }
