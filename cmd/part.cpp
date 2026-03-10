145a57a712a63d1108a6cd52531b8792178a19b8
cesaltinofelix <cesaltinofelix2000@gmail.com>
2026-03-10 19:55:14 +0100

    refactor cmdPart and remove_to_chanel to improve client disconnection handling and message broadcasting

--- cmd/part.cpp
+++ cmd/part.cpp
@@ -4,13 +4,64 @@ void Server::remove_to_chanel(Client *cl) {
   for (std::map<std::string, Channel *>::iterator it = _channels.begin();
      it != _channels.end();) {
     Channel *chan = it->second;
+    std::string nick = cl->getNickname();
+    bool wasInChannel = chan->hasClient(nick);
     chan->removeClient(cl);
+    chan->removeOperator(nick);
+    if (wasInChannel && !chan->isEmpty()) {
+      std::string partMsg = ":" + nick + " QUIT :Client disconnected\r\n";
+      chan->broadcast(partMsg);
+    }
     if (chan->isEmpty()) {
       delete chan;
-      std::map<std::string, Channel *>::iterator toErase = it++;
-      _channels.erase(toErase);
+      _channels.erase(it++);
     } else {
       ++it;
     }
+  }
+}
+
+void Server::cmdPart(int fd, const std::string &args)
+{
+  if (args.empty()) {
+    sendToClient(fd, "461 PART :Not enough parameters");
+    return;
+  }
+
+  std::string channelName = args;
+  std::string reason = "";
+  size_t sp = args.find(' ');
+  if (sp != std::string::npos) {
+    channelName = args.substr(0, sp);
+    reason = args.substr(sp + 1);
+    if (!reason.empty() && reason[0] == ':')
+      reason.erase(0, 1);
+  }
+
+  Channel *channel = getChannel(channelName);
+  if (!channel) {
+    sendToClient(fd, "403 " + channelName + " :No such channel");
+    return;
+  }
+
+  std::string nick = getNickByFd(fd);
+  if (!channel->hasClient(nick)) {
+    sendToClient(fd, "442 " + channelName + " :You're not on that channel");
+    return;
+  }
+
+  std::string partMsg = ":" + nick + " PART " + channelName;
+  if (!reason.empty())
+    partMsg += " :" + reason;
+  partMsg += "\r\n";
+  channel->broadcast(partMsg);
+
+  Client *client = _clients[fd];
+  channel->removeClient(client);
+  channel->removeOperator(nick);
+
+  if (channel->isEmpty()) {
+    delete channel;
+    _channels.erase(channelName);
   }
 }
